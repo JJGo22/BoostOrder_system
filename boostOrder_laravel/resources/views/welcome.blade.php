ef01bbf9137c7a3cfda9b5dc9fa8dd9db9339294
jianji <jianjI@simitgroup.com>
2023-02-06 09:00:39 +0800

    first version

---
+++ boostOrder_laravel/resources/views/welcome.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+
+<div class="container text-center">
+
+  <div class="row">
+    <div class="col">
+          <img src="https://upload.wikimedia.org/wikipedia/commons/3/3c/IMG_logo_%282017%29.svg" style="width:100px;height:100px;"/>
+          <div class="grid">
+              <div class="col-12">Vin:</div>
+              <div class="col-12">Year:</div>
+              <div class="col-12">Brand: </div>
+              <div class="col-12">Color: blue</div>
+          </div>
+    </div>
+    <div class="col">
+          <img src="https://upload.wikimedia.org/wikipedia/commons/3/3c/IMG_logo_%282017%29.svg" style="width:100px;height:100px;"/>
+          <div class="grid">
+              <div class="col-12">Vin:</div>
+              <div class="col-12">Year:</div>
+              <div class="col-12">Brand: </div>
+              <div class="col-12">Color: blue</div>
+          </div>
+    </div>
+    <div class="col">
+          <img src="https://upload.wikimedia.org/wikipedia/commons/3/3c/IMG_logo_%282017%29.svg" style="width:100px;height:100px;"/>
+          <div class="grid">
+              <div class="col-12">Vin:</div>
+              <div class="col-12">Year:</div>
+              <div class="col-12">Brand: </div>
+              <div class="col-12">Color: blue</div>
+          </div>
+    </div>
+  </div>
+
+</div>
+
+</html>
+
